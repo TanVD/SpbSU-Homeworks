b73722e28b0fedec59c33aed2cd1d84777185d42
Vladislav Tankov <vdtankov@gmail.com>
2015-03-13 01:29:33 +0300

    Added Doxygen comments:

--- 2_term/4/4-2/calculatorSimpleUI.h
+++ 2_term/4/4-2/calculatorSimpleUI.h
@@ -16,14 +16,29 @@ public:
     ~CalculatorSimpleUI();
 
 private slots:
+    /**
+     * @brief on_spinBox_valueChanged Sets first argument in calc object if spinbox was changed
+     * @param arg1 First argument
+     */
     void on_spinBox_valueChanged(int arg1);
 
+    /**
+     * @brief on_spinBox_2_valueChanged Sets second argument in calc object if spinbox was changed
+     * @param arg1 Second argument
+     */
     void on_spinBox_2_valueChanged(int arg1);
 
+    /**
+     * @brief on_comboBox_currentIndexChanged Sets operation if combo box was changed
+     * @param index Index of operation
+     */
     void on_comboBox_currentIndexChanged(int index);
 
 private:
     Ui::CalculatorSimpleUI *ui;
+    /**
+     * @brief calc
+     */
     CalculatorSimple* calc;
 
 };
