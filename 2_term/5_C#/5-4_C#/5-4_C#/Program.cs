254347fcd2e27b6a6e1e64040a3284536f55f43d
Vladislav Tankov <vdtankov@gmail.com>
2015-03-20 17:47:44 +0300

    Fixed buffer limits for movements of cursor

--- 2_term/5_C#/5-4_C#/5-4_C#/Program.cs
+++ 2_term/5_C#/5-4_C#/5-4_C#/Program.cs
@@ -8,8 +8,8 @@ namespace _C
         {
             var eventLoop = new EventLoop();
             eventLoop.LeftHandler += (Object sender, EventArgs EventArgs) => {if (Console.CursorLeft > 1) Console.CursorLeft -= 1;};
-            eventLoop.RightHandler += (Object sender, EventArgs EventArgs) =>{if (Console.CursorLeft < Console.BufferWidth) Console.CursorLeft += 1;};
-            eventLoop.DownHandler += (Object sender, EventArgs EventArgs) => {if (Console.CursorTop < Console.BufferHeight) Console.CursorTop += 1;};
+            eventLoop.RightHandler += (Object sender, EventArgs EventArgs) =>{if (Console.CursorLeft < Console.BufferWidth - 1) Console.CursorLeft += 1;};
+            eventLoop.DownHandler += (Object sender, EventArgs EventArgs) => {if (Console.CursorTop < Console.BufferHeight - 1) Console.CursorTop += 1;};
             eventLoop.UpHandler += (Object sender, EventArgs EventArgs) =>{if (Console.CursorTop > 1) Console.CursorTop -= 1;};
             eventLoop.Run();
         }
