b370a32e48d1009f2c8695318326e72a485ffdfa
Vladislav Tankov <vdtankov@gmail.com>
2015-03-20 17:27:18 +0300

    Minor style improvements

--- 2_term/5_C#/5-1_C#/5-1_C#_Test/Test.cs
+++ 2_term/5_C#/5-1_C#/5-1_C#_Test/Test.cs
@@ -11,12 +11,11 @@ namespace _C_Test
         [Test()]
         public void MapListTest()
         {
-            List<int> list = new List<int>();
-            list.Add(5);
-            list.Add(7);
+            List<int> list = new List<int>() {5, 7};
             List<int> listNew = MapClass<int>.Map(list, x => x * x);
             Assert.IsTrue(listNew.Exists(x => x == 25));
             Assert.IsTrue(listNew.Exists(x => x == 49));
+            Assert.IsFalse(listNew.Exists(x => x == 4));
         }
     }
 }
