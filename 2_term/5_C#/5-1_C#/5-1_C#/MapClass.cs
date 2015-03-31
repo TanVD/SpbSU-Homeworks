f57d5bc81fcaeb48c8145b089ae16110baba88e4
Vladislav Tankov <vdtankov@gmail.com>
2015-04-01 01:20:38 +0300

    Added comments

--- 2_term/5_C#/5-1_C#/5-1_C#/MapClass.cs
+++ 2_term/5_C#/5-1_C#/5-1_C#/MapClass.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 namespace _C
 {
+    /// <summary>
+    /// Map class is used to create new list applying function to every element of original list.
+    /// </summary>
     public class MapClass<T>
     {
+        /// <summary>
+        /// Creates new list applying lambda function to every element of old list
+        /// </summary>
+        /// <param name="list">Original list.</param>
+        /// <param name="lambda">Lambda to apply to every element of original list.</param>
         public static List<T> Map(List<T> list, Func<T, T> lambda)
         {
             List<T> newList = new List<T>();
