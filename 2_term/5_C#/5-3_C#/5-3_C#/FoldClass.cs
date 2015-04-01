79541a5ed8878d2e75675f17d78e6da986139cc6
Vladislav Tankov <vdtankov@gmail.com>
2015-04-01 11:25:56 +0300

    Added comments

--- 2_term/5_C#/5-3_C#/5-3_C#/FoldClass.cs
+++ 2_term/5_C#/5-3_C#/5-3_C#/FoldClass.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 
 namespace _C
 {
+    /// <summary>
+    /// Fold class implements function accumulating by some rule values of list
+    /// </summary>
     public class FoldClass<T>
     {
+        /// <summary>
+        /// Function accumulating by some rule values of list
+        /// </summary>
+        /// <param name="list">List to fold.</param>
+        /// <param name="start">Starting value for function.</param>
+        /// <param name="accamFunction">Rule used for accumulating.</param>
+        /// <returns>Accumulated value.</returns>
         public static T Fold(List<T> list, T start, Func<T, T, T> accamFunction)
         {
             T result = start;
