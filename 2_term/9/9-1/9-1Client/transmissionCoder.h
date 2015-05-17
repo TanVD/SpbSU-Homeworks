efb521c6a4552e5ca91261e6770a9115fed3162f
Vladislav Tankov <vdtankov@gmail.com>
2015-05-17 20:48:09 +0300

    Added comments

--- 2_term/9/9-1/9-1Client/transmissionCoder.h
+++ 2_term/9/9-1/9-1Client/transmissionCoder.h
@@ -5,7 +5,18 @@
 class TransmissionCoder
 {
 public:
+    /**
+     * @brief decodeFromTransmission Decodes byte transmission in QString
+     * @param connection Socket which get incoming connection
+     * @return QString if decoding was successful, else exception "Unreliable connection" type
+     */
     static QString decodeFromTransmission(QTcpSocket* connection, quint16 &blockSize);
+
+    /**
+     * @brief codeForTransmission Code QString in byte array for transmission
+     * @param stringToCode String to code
+     * @return QByteArray - coded string
+     */
     static QByteArray codeForTransmission(const QString &stringToCode);
 };
 
