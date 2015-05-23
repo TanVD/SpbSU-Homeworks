ac5da5191af96dcdd754c783a6b2fd2441055326
Vladislav Tankov <vdtankov@gmail.com>
2015-05-23 18:12:54 +0300

    Added tests

---
+++ 2_term/3/3-1/quickSort.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "sortInterface.h"
+/**
+ * @brief The QuickSort class sorting using cormen quick sort algorithm
+ */
+class QuickSort : public SortInterface
+{
+public:
+    /**
+     * @brief QuickSort constructor
+     * @param ArrayToSort array which will be sorted by ref
+     * @param LengthArrayToSort length of array to sort
+     */
+    QuickSort(int* arrayToSort, int lengthArrayToSort);
+
+    /**
+     * @brief SortArray implementation of virtual interface function of sorting
+     * @detailed Quicksort function which uses median random cormen algorithm
+     */
+    void sortArray();
+private:
+    int* array;
+    int lengthArray;
+    void quickSort(int *a, int start, int end);
+    int makeParts(int *a, int start, int strongPoint);
+};
+
