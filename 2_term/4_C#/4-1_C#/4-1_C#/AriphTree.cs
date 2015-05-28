022099fe67335667591691a77d530277968e069c
Vladislav Tankov <vdtankov@gmail.com>
2015-05-28 11:04:11 +0300

    Formatted style

--- 2_term/4_C#/4-1_C#/4-1_C#/AriphTree.cs
+++ 2_term/4_C#/4-1_C#/4-1_C#/AriphTree.cs
@@ -2,175 +2,176 @@ using System;
 
 namespace _C
 {
-    /// <summary>
-    /// Ariph tree class implements binary tree structure.
-    /// </summary>
-    public class AriphTree
-    {
-        private AriphTreeElement head = null;
+	/// <summary>
+	/// Ariph tree class implements binary tree structure.
+	/// </summary>
+	public class AriphTree
+	{
+		private AriphTreeElement head = null;
         
-        private AriphTreeElement current = null;
+		private AriphTreeElement current = null;
 
-        public AriphTree(char value)
-        {
-            head = AriphTreeElement.GetAriphTreeElement(value, null);
-            current = head;
-        }
+		public AriphTree (char value)
+		{
+			head = AriphTreeElement.GetAriphTreeElement (value, null);
+			current = head;
+		}
 
-        /// <summary>
-        /// Сreates the left element of the current node.
-        /// </summary>
-        /// <param name="value">Value to put in created left element of the left node.</param>
-        public void СreateLeftElement(char value)
-        {
-			current.LeftElement = AriphTreeElement.GetAriphTreeElement(value, current);
-        }
+		/// <summary>
+		/// Сreates the left element of the current node.
+		/// </summary>
+		/// <param name="value">Value to put in created left element of the left node.</param>
+		public void СreateLeftElement (char value)
+		{
+			current.LeftElement = AriphTreeElement.GetAriphTreeElement (value, current);
+		}
 
-        /// <summary>
-        /// Сreates the right element of the current node.
-        /// </summary>
-        /// <param name="value">Value to put in created left element of the right node.</param>
-        public void СreateRightElement(char value)
-        {
-			current.RightElement = AriphTreeElement.GetAriphTreeElement(value, current);
-        }
+		/// <summary>
+		/// Сreates the right element of the current node.
+		/// </summary>
+		/// <param name="value">Value to put in created left element of the right node.</param>
+		public void СreateRightElement (char value)
+		{
+			current.RightElement = AriphTreeElement.GetAriphTreeElement (value, current);
+		}
 
-        /// <summary>
-        /// Moves current node to the left child of current now.
-        /// </summary>
-        public void DownLeft()
-        {
-            current = current.LeftElement;
-        }
+		/// <summary>
+		/// Moves current node to the left child of current now.
+		/// </summary>
+		public void DownLeft ()
+		{
+			current = current.LeftElement;
+		}
 
-        /// <summary>
-        /// Moves current node to the right child of current now.
-        /// </summary>
-        public void DownRight()
-        {
-            current = current.RightElement;
-        }
+		/// <summary>
+		/// Moves current node to the right child of current now.
+		/// </summary>
+		public void DownRight ()
+		{
+			current = current.RightElement;
+		}
 
-        /// <summary>
-        /// Moves current node to the ancestor of current now.
-        /// </summary>
-        public void Up()
-        {
-            current = current.Ancestor;
-        }
+		/// <summary>
+		/// Moves current node to the ancestor of current now.
+		/// </summary>
+		public void Up ()
+		{
+			current = current.Ancestor;
+		}
 
-        /// <summary>
-        /// Determines whether left child of current node is free.
-        /// </summary>
-        /// <returns><c>true</c> if left child of current node is free; otherwise, <c>false</c>.</returns>
-        public bool IsLeftFree()
-        {
-            return current.LeftElement == null;
-        }
+		/// <summary>
+		/// Determines whether left child of current node is free.
+		/// </summary>
+		/// <returns><c>true</c> if left child of current node is free; otherwise, <c>false</c>.</returns>
+		public bool IsLeftFree ()
+		{
+			return current.LeftElement == null;
+		}
 
-        /// <summary>
-        /// Determines whether right child of current node is free.
-        /// </summary>
-        /// <returns><c>true</c> if right child of current node is free; otherwise, <c>false</c>.</returns>
-        public bool IsRightFree()
-        {
-            return current.RightElement == null;
-        }
+		/// <summary>
+		/// Determines whether right child of current node is free.
+		/// </summary>
+		/// <returns><c>true</c> if right child of current node is free; otherwise, <c>false</c>.</returns>
+		public bool IsRightFree ()
+		{
+			return current.RightElement == null;
+		}
 
-        /// <summary>
-        /// Sets the current node to head.
-        /// </summary>
-        public void SetDefault()
-        {
-            current = head;
-        }
+		/// <summary>
+		/// Sets the current node to head.
+		/// </summary>
+		public void SetDefault ()
+		{
+			current = head;
+		}
 
-        /// <summary>
-        /// Calculates the tree.
-        /// </summary>
-        /// <returns>Int representing value of tree equation.</returns>
-        public int CalculateTree()
-        {
-            return head.Calculate();
-        }
+		/// <summary>
+		/// Calculates the tree.
+		/// </summary>
+		/// <returns>Int representing value of tree equation.</returns>
+		public int CalculateTree ()
+		{
+			return head.Calculate ();
+		}
 
-        /// <summary>
-        /// Returns value of current node.
-        /// </summary>
-        /// <returns>Value of current node.</returns>
-        public int ValueCurrent()
-        {
-            return current.Value;
-        }
+		/// <summary>
+		/// Returns value of current node.
+		/// </summary>
+		/// <returns>Value of current node.</returns>
+		public int ValueCurrent ()
+		{
+			return current.Value;
+		}
 
-        /// <summary>
-        /// Creates string representing structure of tree.
-        /// </summary>
-        /// <returns>string representing structure of tree.</returns>
-        public string PrintTree()
-        {
-            string stringPrint = "";
-            head.Print(ref stringPrint);
-            return stringPrint;
-        }
+		/// <summary>
+		/// Creates string representing structure of tree.
+		/// </summary>
+		/// <returns>string representing structure of tree.</returns>
+		public string PrintTree ()
+		{
+			string stringPrint = "";
+			head.Print (ref stringPrint);
+			return stringPrint;
+		}
 
-        private class AriphTreeElement
-        {
-			public static AriphTreeElement GetAriphTreeElement(char value, AriphTreeElement ancestor)
-            {
-				if (Char.IsDigit(value))
-				{
-					return new AriphTreeElementNumber(value, ancestor);
+		private class AriphTreeElement
+		{
+			public static AriphTreeElement GetAriphTreeElement (char value, AriphTreeElement ancestor)
+			{
+				if (Char.IsDigit (value)) {
+					return new AriphTreeElementNumber (value, ancestor);
 				}
-				switch (value)
-				{
+				switch (value) {
 				case '+':
 					{
-						return new AriphTreeElementPlus(value, ancestor);
+						return new AriphTreeElementPlus (value, ancestor);
 						break;
 					}
 				case '-':
 					{
-						return new AriphTreeElementMinus(value, ancestor);
+						return new AriphTreeElementMinus (value, ancestor);
 						break;
 					}
 				case '*':
 					{
-						return new AriphTreeElementMultiply(value, ancestor);
+						return new AriphTreeElementMultiply (value, ancestor);
 						break;
 					}
 				case '/':
 					{
-						return new AriphTreeElementDivide(value, ancestor);
+						return new AriphTreeElementDivide (value, ancestor);
 						break;
 					}
 				}
 				return null;
-            }
+			}
 
-            public AriphTreeElement LeftElement{ set; get; }
-            public AriphTreeElement RightElement{ set; get; }
-            public AriphTreeElement Ancestor{ set; get; }
-            public char Value { set; get; }
+			public AriphTreeElement LeftElement{ set; get; }
 
-            public void Print(ref string stringPrint)
-            {
-                if (LeftElement != null)
-                    LeftElement.Print(ref stringPrint);
-                stringPrint = stringPrint + Value;
-                if (RightElement != null)
-                    RightElement.Print(ref stringPrint);
-            }
+			public AriphTreeElement RightElement{ set; get; }
 
-			public virtual int Calculate() 
+			public AriphTreeElement Ancestor{ set; get; }
+
+			public char Value { set; get; }
+
+			public void Print (ref string stringPrint)
+			{
+				if (LeftElement != null)
+					LeftElement.Print (ref stringPrint);
+				stringPrint = stringPrint + Value;
+				if (RightElement != null)
+					RightElement.Print (ref stringPrint);
+			}
+
+			public virtual int Calculate ()
 			{
 				return 0;
 			}
-        }
+		}
 
 		private class AriphTreeElementPlus : AriphTreeElement
 		{
-			public AriphTreeElementPlus(char value, AriphTreeElement ancestor)
+			public AriphTreeElementPlus (char value, AriphTreeElement ancestor)
 			{
 				Value = value;
 				Ancestor = ancestor;
@@ -178,15 +179,15 @@ namespace _C
 				RightElement = null;
 			}
 
-			public override int Calculate()
+			public override int Calculate ()
 			{
-				return LeftElement.Calculate() + RightElement.Calculate();
+				return LeftElement.Calculate () + RightElement.Calculate ();
 			}
 		}
 
 		private class AriphTreeElementMinus : AriphTreeElement
 		{
-			public AriphTreeElementMinus(char value, AriphTreeElement ancestor)
+			public AriphTreeElementMinus (char value, AriphTreeElement ancestor)
 			{
 				Value = value;
 				Ancestor = ancestor;
@@ -194,15 +195,15 @@ namespace _C
 				RightElement = null;
 			}
 
-			public override int Calculate()
+			public override int Calculate ()
 			{
-				return LeftElement.Calculate() - RightElement.Calculate();
+				return LeftElement.Calculate () - RightElement.Calculate ();
 			}
 		}
 
 		private class AriphTreeElementMultiply : AriphTreeElement
 		{
-			public AriphTreeElementMultiply(char value, AriphTreeElement ancestor)
+			public AriphTreeElementMultiply (char value, AriphTreeElement ancestor)
 			{
 				Value = value;
 				Ancestor = ancestor;
@@ -210,15 +211,15 @@ namespace _C
 				RightElement = null;
 			}
 
-			public override int Calculate()
+			public override int Calculate ()
 			{
-				return LeftElement.Calculate() * RightElement.Calculate();
+				return LeftElement.Calculate () * RightElement.Calculate ();
 			}
 		}
 
 		private class AriphTreeElementDivide : AriphTreeElement
 		{
-			public AriphTreeElementDivide(char value, AriphTreeElement ancestor)
+			public AriphTreeElementDivide (char value, AriphTreeElement ancestor)
 			{
 				Value = value;
 				Ancestor = ancestor;
@@ -226,15 +227,15 @@ namespace _C
 				RightElement = null;
 			}
 
-			public override int Calculate()
+			public override int Calculate ()
 			{
-				return LeftElement.Calculate() / RightElement.Calculate();
+				return LeftElement.Calculate () / RightElement.Calculate ();
 			}
 		}
 
 		private class AriphTreeElementNumber : AriphTreeElement
 		{
-			public AriphTreeElementNumber(char value, AriphTreeElement ancestor)
+			public AriphTreeElementNumber (char value, AriphTreeElement ancestor)
 			{
 				Value = value;
 				Ancestor = ancestor;
@@ -242,11 +243,11 @@ namespace _C
 				RightElement = null;
 			}
 
-			public override int Calculate()
+			public override int Calculate ()
 			{
 				return Value - '0';
 			}
 		}
-    }
+	}
 }
 
