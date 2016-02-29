4d26e43e5652d5b7ce614b309684a883ad0c674a
Vladislav Tankov <vdtankov@gmail.com>
2016-02-29 21:54:30 +0300

    Added signatures

--- 2_year/2_term/3/3-4/treeHeight.hs
+++ 2_year/2_term/3/3-4/treeHeight.hs
@@ -1,14 +1,18 @@
+
+data Tree = EmptyTree | Node Int Tree Tree deriving (Show)
+
 --Simple binary tree
+treeInsert :: Int -> Tree -> Tree
 treeInsert x EmptyTree = Node x EmptyTree EmptyTree
 treeInsert x (Node a left right)
 	| x == a = Node x left right
 	| x < a = Node a (treeInsert x left) right
 	| x > a = Node a left (treeInsert x right)
 
-data Tree = EmptyTree | Node Int Tree Tree deriving (Show)
-
+treeHeight :: Tree -> Int
 treeHeight tree  = treeHeight' tree 0
 
+treeHeight' :: Tree -> Int -> Int
 treeHeight' EmptyTree n = n
 treeHeight' (Node x left right) n 
 	| leftHeight > rightHeight  = leftHeight
@@ -17,8 +21,10 @@ treeHeight' (Node x left right) n
 		leftHeight = treeHeight' left (n + 1)
 		rightHeight = treeHeight' right (n + 1)
 
+minTreeHeight :: Tree -> Int
 minTreeHeight tree = minTreeHeight' tree 0
 
+minTreeHeight' :: Tree -> Int -> Int
 minTreeHeight' EmptyTree n = n
 minTreeHeight' (Node x left right) n
 	| leftHeight < rightHeight = leftHeight
