f4d1447b450fd744fcb29ad2ffa26f9fa30eed28
Vladislav Tankov <vdtankov@gmail.com>
2016-02-29 22:09:00 +0300

    Added signatures

--- 2_year/2_term/3/3-2/knotIt.hs
+++ 2_year/2_term/3/3-2/knotIt.hs
@@ -1,8 +1,11 @@
+generateSequence :: [String]
 generateSequence = "1" : "7" : "9" : zipWith (++)  (repl3 generateSequence) (inf3 generateSequence)
 
+inf3 :: [String] -> [String]
 inf3 [] = []
 inf3 (x:y:z:xs) = x:y:z:inf3(x:y:z:xs)
 
+repl3 :: [String] -> [String]
 repl3 [] = []
 repl3 (x:xs) = x:x:x:repl3 xs
 --It seems not possible to do it another way. We always need to add 1 or 7 or 9 which means we need to take from the head of the list
