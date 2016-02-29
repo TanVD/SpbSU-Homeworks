35084b79d74c93395390b7ea202a58a0b1b4e664
Vladislav Tankov <vdtankov@gmail.com>
2016-02-29 21:57:56 +0300

    Added signature

--- 2_year/2_term/3/3-3/listSum.hs
+++ 2_year/2_term/3/3-3/listSum.hs
@@ -1,5 +1,7 @@
+listSum :: [Int] -> Int
 listSum xs = listSum' xs 0 0 0
 
+listSum' :: [Int] -> Int -> Int -> Int ->Int
 listSum' (x:y:z:ys) n index max
 	| (x + z) > max = listSum' (y:z:ys) (n + 1) (n + 1) (x + z)
 	| (x + z) <= max = listSum' (y:z:ys) (n + 1) index max
