f96e8b002e6b67f007a896d3549fd81172ff070e
Vladislav Tankov <vdtankov@gmail.com>
2016-02-29 10:57:35 +0300

    Excersise 3-2

---
+++ 2_year/2_term/3/3-3/listSum.hs
@@ -0,0 +1,6 @@
+listSum xs = listSum' xs 0 0 0
+
+listSum' (x:y:z:ys) n index max
+	| (x + z) > max = listSum' (y:z:ys) (n + 1) (n + 1) (x + z)
+	| (x + z) <= max = listSum' (y:z:ys) (n + 1) index max
+listSum' _ n index max = index
