d2ed348ca00dea9e393485470d296cca105136ed
Vladislav Tankov <vdtankov@gmail.com>
2016-03-25 12:51:03 +0300

    Added maybe

--- 2_year/2_term/Test1/3/3.hs
+++ 2_year/2_term/Test1/3/3.hs
@@ -1,18 +1,17 @@
 import  Data.List
 
---Didn't have time to add Maybe and parametrize
-getPosByFold :: [Int] -> Int -> Int
+getPosByFold :: [Int] -> Int -> Maybe Int
 getPosByFold as a = getIndexFirst (foldl findFunc (0, 0, a) as)
 	where 
 		findFunc (b, c, d) a
 			| (c /= 1) && (d /= a) = (b + 1, c, d)
-			| (c /= 1) && (d == a) = (b + 1, 1, d)
+			| (c /= 1) && (d == a) = (b, 1, d)
 			| c == 1 = (b, c, d)
 
 
-getIndexFirst :: (Int, Int, Int) -> Int
-getIndexFirst (_, 0, _) = -1
-getIndexFirst (x, _, _) = x - 1
+getIndexFirst :: (Int, Int, Int) -> Maybe Int
+getIndexFirst (_, 0, _) = Nothing
+getIndexFirst (x, _, _) = Just x
 
 
 --or just
