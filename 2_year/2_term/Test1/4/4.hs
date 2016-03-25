86e1d5f199f0a898919e22ce67ea1f4bccd9f1cb
Vladislav Tankov <vdtankov@gmail.com>
2016-03-25 12:46:14 +0300

    Added own sum'

--- 2_year/2_term/Test1/4/4.hs
+++ 2_year/2_term/Test1/4/4.hs
@@ -1,2 +1,5 @@
 sumAndDiv :: [Double] -> Double
-sumAndDiv as = (foldl (+) 0 as) / (foldl (\x y -> x * cos y) 1 as)
+sumAndDiv as = sum' as / (foldl (\x y -> x * cos y) 1 as)
+
+sum' :: [Double] -> Double
+sum' as = foldl (+) 0 as
