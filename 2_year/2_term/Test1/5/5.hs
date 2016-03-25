f1ddd2271da11388df8bf6c8579ec55160ea5507
Vladislav Tankov <vdtankov@gmail.com>
2016-03-25 12:42:31 +0300

    Added name fields

--- 2_year/2_term/Test1/5/5.hs
+++ 2_year/2_term/Test1/5/5.hs
@@ -1,7 +1,18 @@
-data Printed = Book String String Int | Journal String Int Int Int
+data Printed = Book {
+					name :: String,
+					author:: String,
+					price:: Int
+					}
+			    | Journal 
+			    	{
+			    	name :: String,
+			    	year :: Int,
+			    	issue :: Int,
+			   		price :: Int
+			   	}
 
 getPrice :: [Printed] -> Int
 getPrice as = foldl funcSum 0 as
 	where 
-		funcSum a (Book _ _ price) = a + price
-		funcSum a (Journal _  _ _ price) = a + price
+		funcSum a b = a + price(b)
+		--funcSum a (Journal _  _ _ price) = a + price
