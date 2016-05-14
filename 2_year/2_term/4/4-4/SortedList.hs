6d0accc8360b42d9d4cde2c529cb850578ea9fd4
Vladislav Tankov <vdtankov@gmail.com>
2016-05-14 23:39:17 +0300

    1.Added solution

---
+++ 2_year/2_term/4/4-4/SortedList.hs
@@ -0,0 +1,45 @@
+main :: IO()
+main = mainProcedure []
+
+mainProcedure :: [Int] -> IO()
+mainProcedure args = do
+    showVariants
+    variant <- getLine
+    case read variant  :: Int of
+        0 -> return()
+        1 -> addNumber args
+        2 -> removeNumber args
+        3 -> printNumbers args args
+
+showVariants :: IO()
+showVariants = do
+    putStrLn "Enter your choice:"
+    putStrLn "0 - exit"
+    putStrLn "1 - addNumber"
+    putStrLn "2 - removeNumber"
+    putStrLn "3 - printNumbers"
+
+addNumber :: [Int] -> IO()
+addNumber list = do
+    str <- getLine
+    let number = read str :: Int
+    mainProcedure $ resort (number : list)
+
+removeNumber :: [Int] -> IO()
+removeNumber list = do
+    str <- getLine
+    let number = read str :: Int
+    mainProcedure $ filter (/= number) list
+
+printNumbers :: [Int] -> [Int] -> IO()
+printNumbers [] list = mainProcedure list
+printNumbers (a:as) list = do
+    putStrLn (" " ++ show a)
+    printNumbers as list
+
+resort :: [Int] -> [Int]
+resort [] = []
+resort [x] = [x]
+resort (a:b:xs)
+    | a < b = a:b:xs
+    | otherwise = b:resort(a:xs)
