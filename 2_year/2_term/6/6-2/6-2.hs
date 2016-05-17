8d7f23c4835bad5a666cdc99e3b8310577e9158f
Vladislav Tankov <vdtankov@gmail.com>
2016-05-17 16:58:11 +0300

    Now way is a result

--- 2_year/2_term/6/6-2/6-2.hs
+++ 2_year/2_term/6/6-2/6-2.hs
@@ -3,7 +3,8 @@ import Prelude hiding (id)
 data Point = Point { 
     id :: Int,
     wasVisited :: Bool,
-    weightPoint :: Int
+    weightPoint :: Int,
+    way :: [Edge]
 } deriving Show
 
 instance Eq Point where
@@ -20,26 +21,37 @@ data Graph = Graph {
     getEdges :: [Edge]
 } deriving Show
 
-data Way = Way [Edge]
+data Way = Way {
+    edges :: [Edge]
+} deriving Show
 {-
-let a = Graph [(Point 1 False 0), (Point 2 False 0), (Point 3 False 0)] [(Edge (Point 1 False 0) (Point 2 False 0) 2), (Edge (Point 2 False 0) (Point 3 False 0) 3), (Edge (Point 1 False 0) (Point 3 False 0) 8)] 
-let b = [(Point 1 False 0), (Point 2 False 0), (Point 3 False 0)]
+let a = Graph [(Point 1 False (0) []), (Point 2 False (0) []), (Point 3 False (0) [])] [(Edge (Point 1 False 0 []) (Point 2 False 0 []) 2), (Edge (Point 2 False 0 []) (Point 3 False 0 []) 3), (Edge (Point 1 False 0 []) (Point 3 False 0 []) 8)] 
+let b = [(Point 1 False 0 []), (Point 2 False 0 []), (Point 3 False 0 [])]
 -}
 
-dejkstra :: Graph -> Graph
-dejkstra (Graph as bs) = dejkstra' (getMin as) (Graph as bs)
+printWay :: [Point] -> Point -> Way
+printWay (a:as) point 
+    | a == point =  Way (way a)
+    | otherwise = printWay as point
+printWay [] point = Way []
+
+dejkstra :: Graph -> Int -> Way
+dejkstra (Graph as bs) idEnd = printWay (getPoints (dejkstra' start (Graph as bs))) end
+    where 
+        start = (Point 1 False (0) [])
+        end = Point idEnd False 0 []
 
 dejkstra' :: Point -> Graph -> Graph
-dejkstra' (Point 0 True 0) graph = graph
+dejkstra' (Point 0 True 0 []) graph = graph
 dejkstra' point (Graph points edges) = dejkstra' (getMin (getPoints newGraph)) newGraph
     where newGraph = updateGraph (Graph points edges) point
 
 getMin :: [Point] -> Point
-getMin as = getMin' as (-1) (Point 0 True 0)
+getMin as = getMin' as (-1) (Point 0 True 0 [])
     where 
         getMin' [] _ point = point
-        getMin' (Point pId pWasVisited weight : bs) n point
-            | (weight < n || n == (-1)) && not pWasVisited = getMin' bs weight (Point pId pWasVisited weight)
+        getMin' (Point pId pWasVisited weight pWay : bs) n point
+            | (weight < n || n == (-1)) && not pWasVisited = getMin' bs weight (Point pId pWasVisited weight pWay)
             | otherwise = getMin' bs n point
 
 updateGraph :: Graph -> Point -> Graph
@@ -47,15 +59,19 @@ updateGraph (Graph as bs) point = Graph (updatePoints (Graph as bs) point) bs
     
 updatePoints :: Graph -> Point -> [Point]
 updatePoints (Graph (c:cs) es) point
-    | c == point =  Point (id point) True (weightPoint point) : updatePoints (Graph cs es) point
-    | not (wasVisited c) = Point (id c) (wasVisited c) (getNewWeight c point es) : updatePoints (Graph cs es) point
-    | otherwise =  Point (id c) (wasVisited c) (weightPoint c) : updatePoints (Graph cs es) point
+    | c == point =  Point (id point) True (weightPoint point) (way point) : updatePoints (Graph cs es) point
+    | not (wasVisited c) && c /= point = Point (id c) (wasVisited c)  (fst updatedWeight) (snd updatedWeight) : updatePoints (Graph cs es) point
+    | otherwise =  Point (id c) (wasVisited c) (weightPoint c) (way c) : updatePoints (Graph cs es) point
+    where
+        updatedWeight = updateWeight c point es
 updatePoints (Graph [] _) _ = []
 
 --first where to go, second from where
-getNewWeight :: Point -> Point -> [Edge] -> Int
-getNewWeight a point (edge:edges)
-    | a == from edge && point == to edge =  weightPoint (to edge) + weightEdge edge
-    |  a == to edge && point == from edge =  weightPoint (from edge) + weightEdge edge
-    |otherwise = getNewWeight a point edges
-getNewWeight a _ [] = weightPoint a
+updateWeight :: Point -> Point -> [Edge] -> (Int, [Edge])
+updateWeight a point (edge:edges) 
+    | a == from edge && point == to edge && ((weightPoint point + weightEdge edge) < weightPoint a || (weightPoint a == (0))) =
+      (weightPoint point + weightEdge edge, (edge : (way point)))
+    | a == to edge && point == from edge && ((weightPoint point + weightEdge edge) < weightPoint a || (weightPoint a == (0))) = 
+      (weightPoint point + weightEdge edge, (edge : (way point)))
+    | otherwise = updateWeight a point edges
+updateWeight a _ [] = (weightPoint a, way a)
